4649cb8f64fde32270a354d662a7fb646ccc6a38
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-11 22:17:28 -0400

    added location schema

--- migrations/20200411213748_create-location.js
+++ migrations/20200411213748_create-location.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('location', (table) => {
+    table.string('weather_zipcode', 5).notNullable().primary().references('zipcode').inTable('weather'); /* PK / FK (not sure if this is correct)*/
+    table.string('city');
+    table.string('state');
+    table.decimal('latitude', 9, 6);
+    table.decimal('longitude', 9, 6);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('location');
 };
