a0ca0acb2436857dffe9474d6efa987b61c3899c
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-11 21:33:10 -0400

    added users schema

--- migrations/20200410214703_create-users.js
+++ migrations/20200410214703_create-users.js
@@ -1,7 +1,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
-    table.increments(); // id PK serial
-    table.string('username').notNullable();
+    table.string('email', 50).notNullable().primary(); /* PK */
+    table.string('display_name'); /* Nullable */
+    table.string('weather_zipcode', 5).references('zipcode').inTable('weather'); /* FK */
   });
 };
 
