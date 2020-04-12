7ea302299e8a5fe2d0e503a11cdbb95c9a934abf
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-11 22:10:56 -0400

    added weather schema

--- migrations/20200411213654_create-weather.js
+++ migrations/20200411213654_create-weather.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('weather', (table) => {
+    table.string('zipcode', 5).notNullable().primary(); /* PK */
+    table.decimal('day_max_temp', 5, 2); /* Length, Precision might be off */
+    table.decimal('day_min_temp', 5, 2); 
+    table.decimal('day_total_rain', 5, 2); 
+    table.decimal('forcast1_max_temp', 5, 2); 
+    table.decimal('forcast1_min_temp', 5, 2); 
+    table.decimal('forcast1_total_rain', 5, 2); 
+    table.decimal('forcast2_max_temp', 5, 2); 
+    table.decimal('forcast2_min_temp', 5, 2); 
+    table.decimal('forcast2_total_rain', 5, 2); 
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('weather');
 };
