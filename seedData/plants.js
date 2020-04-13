1523e2b6de29a9272e9f13f01e2ebda56bcac1c6
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-13 17:12:31 -0400

    added seed data for plants

---
+++ seedData/plants.js
@@ -0,0 +1,534 @@
+module.exports = [
+  {
+    plant_id: 1,
+    trefle_id: 193519,
+    common_name: "corn",
+    scientific_name: "Zea mays",
+    duration: "Annual",
+    outdoor_plant: true,
+    images:{
+      "url": "https://upload.wikimedia.org/wikipedia/commons/7/71/Symbol_redirect_arrow_with_gradient.svg"
+    },
+    /* In main_species object */
+    foliage: {
+      "color": "Dark Green",
+      "porosity_summer": "Moderate",
+      "porosity_winter": "Porous",
+      "texture": "Coarse"
+    },
+    fruit_or_seed: {
+      "color": "Yellow",
+      "conspicuous": true,
+      "seed_abundance": "High",
+      "seed_period_begin": "Summer",
+      "seed_period_end": "Summer",
+      "seed_persistence": null
+    },
+    growth: {
+      "anaerobic_tolerance": "None",
+      "caco_3_tolerance": "Medium",
+      "cold_stratification_required": null,
+      "drought_tolerance": "Low",
+      "fertility_requirement": "High",
+      "fire_tolerance": "None",
+      "frost_free_days_minimum": 90.0,
+      "hedge_tolerance": "None",
+      "moisture_use": "High",
+      "ph_maximum": 7.5,
+      "ph_minimum": 5.5,
+      "planting_density_maximum": {
+          "acre": 2.3e4,
+          "sqm": 93077780.0
+      },
+      "planting_density_minimum": {
+          "acre": 1.6e4,
+          "sqm": 64749760.0
+      },
+      "precipitation_maximum": {
+          "cm": 127.00025400050801,
+          "inches": 50.0
+      },
+      "precipitation_minimum": {
+          "cm": 50.800101600203206,
+          "inches": 20.0
+      },
+      "resprout_ability": null,
+      "root_depth_minimum": {
+          "cm": 20.32004064008128,
+          "inches": 8.0
+      },
+      "salinity_tolerance": "Low",
+      "shade_tolerance": "Tolerant",
+      "temperature_minimum": {
+          "deg_c": 0.0,
+          "deg_f": 32.0
+      }
+    },
+    seed: {
+      "bloom_period": "Early Summer",
+      "commercial_availability": "Routinely Available",
+      "seed_spread_rate": "Slow",
+      "seedling_vigor": "High",
+      "seeds_per_pound": 1134.0,
+      "small_grain": true,
+      "vegetative_spread_rate": "None"
+    },
+    specifications: {
+      "bloat": "Medium",
+      "c_n_ratio": "Medium",
+      "coppice_potential": null,
+      "fall_conspicuous": null,
+      "fire_resistance": null,
+      "growth_form": "Single Stem",
+      "growth_habit": "Graminoid",
+      "growth_period": "Summer",
+      "growth_rate": "Rapid",
+      "known_allelopath": null,
+      "leaf_retention": null,
+      "lifespan": null,
+      "low_growing_grass": null,
+      "mature_height": {
+          "cm": 243.82810118866195,
+          "ft": 8.0
+      },
+      "max_height_at_base_age": {
+          "cm": null,
+          "ft": null
+      },
+      "nitrogen_fixation": "None",
+      "regrowth_rate": null,
+      "shape_and_orientation": "Erect",
+      "toxicity": "None"
+    },
+    family_common_name: "Grass family" 
+  },
+
+  {
+    plant_id: 2,
+    trefle_id: 111928,
+    common_name: "white doll's daisy",
+    scientific_name: "Boltonia asteroides",
+    duration: "Perennial",
+    outdoor_plant: true,
+    images:{
+      "url": "https://upload.wikimedia.org/wikipedia/commons/2/2c/Boltonia_latisquama.jpg"
+    },
+    foliage: {
+      "color": "Green",
+      "porosity_summer": "Moderate",
+      "porosity_winter": "Porous",
+      "texture": "Coarse"
+    },
+    fruit_or_seed: {
+      "color": "Brown",
+      "conspicuous": null,
+      "seed_abundance": "High",
+      "seed_period_begin": "Summer",
+      "seed_period_end": "Fall",
+      "seed_persistence": null
+    },
+    growth: {
+      "anaerobic_tolerance": "Medium",
+      "caco_3_tolerance": "Low",
+      "cold_stratification_required": true,
+      "drought_tolerance": "Low",
+      "fertility_requirement": "Medium",
+      "fire_tolerance": "Medium",
+      "frost_free_days_minimum": 80.0,
+      "hedge_tolerance": "None",
+      "moisture_use": "Medium",
+      "ph_maximum": 7.0,
+      "ph_minimum": 5.3,
+      "planting_density_maximum": {
+          "acre": null,
+          "sqm": null
+      },
+      "planting_density_minimum": {
+          "acre": null,
+          "sqm": null
+      },
+      "precipitation_maximum": {
+          "cm": 139.70027940055883,
+          "inches": 55.0
+      },
+      "precipitation_minimum": {
+          "cm": 55.88011176022353,
+          "inches": 22.0
+      },
+      "resprout_ability": null,
+      "root_depth_minimum": {
+          "cm": 30.480060960121925,
+          "inches": 12.0
+      },
+      "salinity_tolerance": "Low",
+      "shade_tolerance": "Intermediate",
+      "temperature_minimum": {
+          "deg_c": -38.888888888888886,
+          "deg_f": -38.0
+      }
+    },
+    seed: {
+      "bloom_period": "Mid Summer",
+      "commercial_availability": "Routinely Available",
+      "seed_spread_rate": "Moderate",
+      "seedling_vigor": "High",
+      "seeds_per_pound": 2.063e6,
+      "small_grain": null,
+      "vegetative_spread_rate": "None"
+    },
+    specifications: {
+      "bloat": "None",
+      "c_n_ratio": "Medium",
+      "coppice_potential": null,
+      "fall_conspicuous": null,
+      "fire_resistance": null,
+      "growth_form": "Single Crown",
+      "growth_habit": "Forb/herb",
+      "growth_period": "Summer",
+      "growth_rate": "Rapid",
+      "known_allelopath": null,
+      "leaf_retention": null,
+      "lifespan": "Moderate",
+      "low_growing_grass": null,
+      "mature_height": {
+          "cm": 243.82810118866195,
+          "ft": 8.0
+      },
+      "max_height_at_base_age": {
+          "cm": null,
+          "ft": null
+      },
+      "nitrogen_fixation": "None",
+      "regrowth_rate": "Slow",
+      "shape_and_orientation": "Erect",
+      "toxicity": "None"
+    },
+    family_common_name: "Aster family" 
+  },
+
+  {
+    plant_id: 3,
+    trefle_id: 155728,
+    common_name: "wild mint",
+    scientific_name: "Mentha arvensis",
+    duration: "Perennial",
+    outdoor_plant: false,
+    images:  {
+      "url": "https://upload.wikimedia.org/wikipedia/commons/8/8c/Mentha_arvensis_-_p%C3%B5ldm%C3%BCnt_Keila.jpg"
+    },
+    foliage: {
+      "color": "Green",
+      "porosity_summer": "Moderate",
+      "porosity_winter": "Porous",
+      "texture": "Medium"
+    },
+    fruit_or_seed: {
+      "color": "Brown",
+      "conspicuous": null,
+      "seed_abundance": "Medium",
+      "seed_period_begin": "Summer",
+      "seed_period_end": "Fall",
+      "seed_persistence": null
+    },
+    growth: {
+      "anaerobic_tolerance": "Medium",
+      "caco_3_tolerance": "Medium",
+      "cold_stratification_required": null,
+      "drought_tolerance": "Low",
+      "fertility_requirement": "Medium",
+      "fire_tolerance": "None",
+      "frost_free_days_minimum": 100.0,
+      "hedge_tolerance": "None",
+      "moisture_use": "Medium",
+      "ph_maximum": 7.0,
+      "ph_minimum": 5.0,
+      "planting_density_maximum": {
+          "acre": 19360.0,
+          "sqm": 78347209.60000001
+      },
+      "planting_density_minimum": {
+          "acre": 3450.0,
+          "sqm": 13961667.0
+      },
+      "precipitation_maximum": {
+          "cm": 127.00025400050801,
+          "inches": 50.0
+      },
+      "precipitation_minimum": {
+          "cm": 50.800101600203206,
+          "inches": 20.0
+      },
+      "resprout_ability": null,
+      "root_depth_minimum": {
+          "cm": 15.240030480060963,
+          "inches": 6.0
+      },
+      "salinity_tolerance": "None",
+      "shade_tolerance": "Intermediate",
+      "temperature_minimum": {
+          "deg_c": -36.11111111111111,
+          "deg_f": -33.0
+      }
+    },
+    seed: {
+      "bloom_period": "Spring",
+      "commercial_availability": "Routinely Available",
+      "seed_spread_rate": "Moderate",
+      "seedling_vigor": "Medium",
+      "seeds_per_pound": null,
+      "small_grain": null,
+      "vegetative_spread_rate": "None"
+    },
+    specifications: {
+      "bloat": "None",
+      "c_n_ratio": null,
+      "coppice_potential": null,
+      "fall_conspicuous": null,
+      "fire_resistance": null,
+      "growth_form": "Single Stem",
+      "growth_habit": "Forb/herb",
+      "growth_period": "Spring",
+      "growth_rate": "Moderate",
+      "known_allelopath": null,
+      "leaf_retention": null,
+      "lifespan": "Short",
+      "low_growing_grass": null,
+      "mature_height": {
+          "cm": 79.24413288631514,
+          "ft": 2.6
+      },
+      "max_height_at_base_age": {
+          "cm": null,
+          "ft": null
+      },
+      "nitrogen_fixation": "None",
+      "regrowth_rate": "Slow",
+      "shape_and_orientation": "Erect",
+      "toxicity": "None"
+    },
+    family_common_name: "Mint family" 
+  },
+
+  {
+    plant_id: 4,
+    trefle_id: 158107,
+    common_name: "true forget-me-not",
+    scientific_name: "Myosotis scorpioides",
+    duration: "Perennial",
+    outdoor_plant: false,
+    images:  {
+      "url": "https://upload.wikimedia.org/wikipedia/commons/9/9f/Myosotis_scorpioides_-_soo-l%C3%B5osilm_Keilas.jpg"
+    },
+    foliage: {
+      "color": "Green",
+      "porosity_summer": "Dense",
+      "porosity_winter": "Porous",
+      "texture": "Fine"
+    },
+    fruit_or_seed: {
+      "color": "Brown",
+      "conspicuous": null,
+      "seed_abundance": "Medium",
+      "seed_period_begin": "Spring",
+      "seed_period_end": "Summer",
+      "seed_persistence": null
+    },
+    growth: {
+      "anaerobic_tolerance": "High",
+      "caco_3_tolerance": "Low",
+      "cold_stratification_required": null,
+      "drought_tolerance": "None",
+      "fertility_requirement": "Medium",
+      "fire_tolerance": "Low",
+      "frost_free_days_minimum": 130.0,
+      "hedge_tolerance": "None",
+      "moisture_use": "High",
+      "ph_maximum": 7.5,
+      "ph_minimum": 5.5,
+      "planting_density_maximum": {
+          "acre": 1.1e4,
+          "sqm": 44515460.0
+      },
+      "planting_density_minimum": {
+          "acre": 4.8e3,
+          "sqm": 19424928.0
+      },
+      "precipitation_maximum": {
+          "cm": 139.70027940055883,
+          "inches": 55.0
+      },
+      "precipitation_minimum": {
+          "cm": 60.96012192024385,
+          "inches": 24.0
+      },
+      "resprout_ability": null,
+      "root_depth_minimum": {
+          "cm": 20.32004064008128,
+          "inches": 8.0
+      },
+      "salinity_tolerance": "None",
+      "shade_tolerance": "Intermediate",
+      "temperature_minimum": {
+          "deg_c": -36.11111111111111,
+          "deg_f": -33.0
+      }
+    },
+    seed: {
+      "bloom_period": "Mid Spring",
+      "commercial_availability": "Routinely Available",
+      "seed_spread_rate": "Rapid",
+      "seedling_vigor": "Medium",
+      "seeds_per_pound": 2.25e5,
+      "small_grain": null,
+      "vegetative_spread_rate": "Moderate"
+    },
+    specifications: {
+      "bloat": "None",
+      "c_n_ratio": "Medium",
+      "coppice_potential": null,
+      "fall_conspicuous": null,
+      "fire_resistance": null,
+      "growth_form": "Rhizomatous",
+      "growth_habit": "Forb/herb",
+      "growth_period": "Spring and Summer",
+      "growth_rate": "Moderate",
+      "known_allelopath": null,
+      "leaf_retention": null,
+      "lifespan": "Short",
+      "low_growing_grass": null,
+      "mature_height": {
+          "cm": 45.71776897287412,
+          "ft": 1.5
+      },
+      "max_height_at_base_age": {
+          "cm": null,
+          "ft": null
+      },
+      "nitrogen_fixation": "None",
+      "regrowth_rate": "Moderate",
+      "shape_and_orientation": "Decumbent",
+      "toxicity": "None"
+    },
+    family_common_name: "Borage family" 
+  },
+
+  {
+    plant_id: 5,
+    trefle_id: 175820,
+    common_name: "cluster rose",
+    scientific_name: "Rosa pisocarpa",
+    duration: "Perennial",
+    outdoor_plant: true,
+    images:   {
+      "url": "https://upload.wikimedia.org/wikipedia/commons/9/93/Diplolepis-rosae.jpg"
+    },
+    foliage: {
+      "color": "Green",
+      "porosity_summer": "Dense",
+      "porosity_winter": "Dense",
+      "texture": "Coarse"
+    },
+    fruit_or_seed: {
+      "color": "Red",
+      "conspicuous": null,
+      "seed_abundance": null,
+      "seed_period_begin": "Spring",
+      "seed_period_end": "Fall",
+      "seed_persistence": null
+    },
+    growth: {
+      "anaerobic_tolerance": "Medium",
+      "caco_3_tolerance": null,
+      "cold_stratification_required": null,
+      "drought_tolerance": "Medium",
+      "fertility_requirement": null,
+      "fire_tolerance": null,
+      "frost_free_days_minimum": 77.0,
+      "hedge_tolerance": null,
+      "moisture_use": null,
+      "ph_maximum": 7.3,
+      "ph_minimum": 5.0,
+      "planting_density_maximum": {
+          "acre": 5120.0,
+          "sqm": 20719923.2
+      },
+      "planting_density_minimum": {
+          "acre": 1280.0,
+          "sqm": 5179980.8
+      },
+      "precipitation_maximum": {
+          "cm": 264.1605283210567,
+          "inches": 104.0
+      },
+      "precipitation_minimum": {
+          "cm": 38.10007620015241,
+          "inches": 15.0
+      },
+      "resprout_ability": null,
+      "root_depth_minimum": {
+          "cm": 30.480060960121925,
+          "inches": 12.0
+      },
+      "salinity_tolerance": "None",
+      "shade_tolerance": "Intolerant",
+      "temperature_minimum": {
+          "deg_c": -31.11111111111111,
+          "deg_f": -24.0
+      }
+    },
+    seed: {
+      "bloom_period": "Summer",
+      "commercial_availability": null,
+      "seed_spread_rate": "Slow",
+      "seedling_vigor": "Low",
+      "seeds_per_pound": 71520.0,
+      "small_grain": null,
+      "vegetative_spread_rate": "Rapid"
+    },
+    specifications: {
+      "bloat": "None",
+      "c_n_ratio": "Medium",
+      "coppice_potential": null,
+      "fall_conspicuous": null,
+      "fire_resistance": null,
+      "growth_form": "Thicket Forming",
+      "growth_habit": "Subshrub",
+      "growth_period": null,
+      "growth_rate": "Rapid",
+      "known_allelopath": null,
+      "leaf_retention": null,
+      "lifespan": "Short",
+      "low_growing_grass": null,
+      "mature_height": {
+          "cm": 91.43553794574824,
+          "ft": 3.0
+      },
+      "max_height_at_base_age": {
+          "cm": 182.87107589149647,
+          "ft": 6.0
+      },
+      "nitrogen_fixation": "None",
+      "regrowth_rate": null,
+      "shape_and_orientation": "Irregular",
+      "toxicity": "None"
+    },
+    family_common_name: "Rose family" 
+  },
+]
+
+/*
+Plants Schema:
+  table.integer('plant_id').primary(); // PK 
+  table.string('common_name'); 
+  table.string('scientific_name');
+  table.integer('trefle_id');
+  table.string('duration');
+  table.boolean('outdoor_plant');
+  table.json('images');
+  table.json('foilage');
+  table.json('fruit_or_seed');
+  table.json('growth');
+  table.json('seed');
+  table.json('specifications');
+  table.string('family_common_name');
+*/
