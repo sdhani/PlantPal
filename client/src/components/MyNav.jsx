0f588bd17a9a422652b3c5fb07d1725961784b90
MarjanGit <mansar16@huntersoe.org>
2020-04-17 19:58:41 -0400

    try a different color home

--- client/src/components/MyNav.jsx
+++ client/src/components/MyNav.jsx
@@ -14,7 +14,7 @@ class MyNav extends Component {
       fontFamily: "Ubuntu, sans-serif",
       fontWeight: "1000",
       color: "white",
-      backgroundColor: "#0f6e2d",
+      backgroundColor: "#158538",
     };
     const stylesLogo = {
       fontSize: "1.8rem",
@@ -22,7 +22,7 @@ class MyNav extends Component {
       fontFamily: "Ubuntu, sans-serif",
       fontWeight: "1000",
       color: "white",
-      backgroundColor: "#0f6e2d",
+      backgroundColor: "#158538",
     };
 
     if (
