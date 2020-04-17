f814c17667346945aea268560d275c92ea9f4e84
MarjanGit <mansar16@huntersoe.org>
2020-04-17 15:46:28 -0400

    displays plant info on plant page, unstyled

--- client/src/components/Plant.jsx
+++ client/src/components/Plant.jsx
@@ -10,13 +10,14 @@ class Plant extends Component {
     const { plant } = this.props.location.state;
     console.log(Object.keys(plant));
     let plantData = Object.keys(plant).map((key) => {
-      return <li>{key}</li>;
+      return <div>{key},</div>;
     });
     console.log(this.props.location.state.plant);
     return (
       <div>
         <h1>Plant Page</h1>
         <ul>{plantData}</ul>
+        <div>{JSON.stringify(plant)}</div>
       </div>
     );
   }
