cdc43b15610362713b63a94142b0f1e93ac6d3ef
MarjanGit <mansar16@huntersoe.org>
2020-04-17 18:38:47 -0400

    image in plant page

--- client/src/components/Plant.jsx
+++ client/src/components/Plant.jsx
@@ -12,11 +12,28 @@ class Plant extends Component {
   render() {
     const { plant } = this.props.location.state;
     let plantData = Object.keys(plant).map((key) => {
-      return <li>{key}</li>;
+      if (typeof plant[key] === "object") {
+        return <li>{key}</li>;
+      } else {
+        return (
+          <div style={{ fontSize: "25px" }}>
+            {key} : {plant[key]}
+          </div>
+        );
+      }
     });
+    const {
+      plant_id,
+      trefle_id,
+      common_name,
+      scientific_name,
+      duration,
+      family_common_name,
+    } = plant;
     return (
-      <div>
-        <h1>Plant Page</h1>
+      <div style={{ margin: 50 }}>
+        <h1>{plant.common_name}</h1>
+        <img src={plant.images.url} style={{ width: "400px" }}></img>
         <ul>{plantData}</ul>
         <div>{JSON.stringify(plant)}</div>
       </div>
