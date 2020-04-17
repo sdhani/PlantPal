10953ef6d49b9fd81e21f37ecbb11ef940ccb0ef
MarjanGit <mansar16@huntersoe.org>
2020-04-17 17:58:30 -0400

    added card links

--- client/src/components/PlantCard.jsx
+++ client/src/components/PlantCard.jsx
@@ -12,34 +12,39 @@ class PlantCard extends Component {
     const img = this.props.plant.images.url;
     const plant = { ...this.props.plant };
     return (
-      <Card
-        style={{
-          width: "18rem",
-          height: this.props.height || "500px",
-          width: this.props.width || "400px",
-        }}
+      <Link
+        to={{ pathname: `/plant/${plant_id}`, state: { plant } }}
+        style={{ textDecoration: "none", color: "black" }}
       >
-        <Card.Img variant="top" src={img} style={{ height: "65%" }} />
-        <Card.Body>
-          <Card.Title>{common_name}</Card.Title>
-          {!this.props.preview && (
-            <div>
-              <Card.Text>
-                Some plant info some plant info Some plant info some plant info
-                Some plant info some plant info
-              </Card.Text>
-              <Link to={{ pathname: `/plant/${plant_id}`, state: { plant } }}>
-                <Button
-                  variant="primary"
-                  style={{ backgroundColor: "#23cf53" }}
-                >
-                  View
-                </Button>
-              </Link>
-            </div>
-          )}
-        </Card.Body>
-      </Card>
+        <Card
+          style={{
+            width: "18rem",
+            height: this.props.height || "500px",
+            width: this.props.width || "400px",
+          }}
+        >
+          <Card.Img variant="top" src={img} style={{ height: "65%" }} />
+          <Card.Body>
+            <Card.Title>{common_name}</Card.Title>
+            {!this.props.preview && (
+              <div>
+                <Card.Text>
+                  Some plant info some plant info Some plant info some plant
+                  info Some plant info some plant info
+                </Card.Text>
+                <Link to={{ pathname: `/plant/${plant_id}`, state: { plant } }}>
+                  <Button
+                    variant="primary"
+                    style={{ backgroundColor: "#23cf53" }}
+                  >
+                    View
+                  </Button>
+                </Link>
+              </div>
+            )}
+          </Card.Body>
+        </Card>
+      </Link>
     );
   }
 }
