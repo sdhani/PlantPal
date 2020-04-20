3a3ef887ded682acb44563c14aa27189225cd24a
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-20 12:22:41 -0400

    added crud garden operations

--- controllers/gardens.js
+++ controllers/gardens.js
@@ -5,7 +5,6 @@ module.exports = {
     return knex('gardens')
     .where('user_id', id)
     .orderBy('id')
-    
   },
 
   createGarden(garden_name, user_id){
@@ -16,58 +15,23 @@ module.exports = {
     })
   },
 
-  // getPlantsFromGarden(garden_id, user_id){
-  //   const garden = knex('plants')
-  //   .where('garden_id', garden_id)
-    
-  //   const permission = knex('gardens').where('user_id', user_id)
-  //   .orderBy('id')
-  // },
+  getGardenByID(user_id, id){
+    return knex('gardens')
+    .where('id', id)
+    .where('user_id', user_id)
+  },
 
-  // getPlantID(id) {
-  //   return knex('plants')
-  //     .select('id')
-  //     .where('username', username);
-  // },
+  updateGardenByID(user_id, id, garden_name){
+    return knex('gardens')
+    .where('id', id)
+    .where('user_id', user_id)
+    .update({garden_name: garden_name})
+  },
 
-  // getAllUserTodos(username) {
-  //   const user_id = knex('plants').select('id').where('username', username);
-  //   return knex('plants')
-  //     .join('todo', 'plants.id', '=', 'todo.user_id')
-  //     .where('user_id', user_id);
-  // },
-
-  // createNewTodo(username, todo) {
-  //   const user_id = knex('plants').select('id').where('username', username);
-  //   todo.user_id = user_id;
-  //   return knex('todo')
-  //     .insert(todo);
-  // },
-
-  // /* Ensures todo belongs to user */
-  // getTodoByID(username, id){
-  //   const user_id = knex('plants').select('id').where('username', username);
-  //   return knex('todo')
-  //     .join('plants', 'todo.user_id', '=', 'plants.id')
-  //     .where('todo.user_id', user_id)
-  //     .where('todo.id', id);
-  // },
-
-  // update(username, id, content, completed){
-  //   const user_id = knex('plants').select('id').where('username', username);
-  //   // const validateUser = knex('todo').join('plants', 'todo.user_id', '=', 'plants.id').where('todo.user_id', user_id).where('todo.id', id);
-  //   return knex('todo')
-  //     .where('id', id)
-  //     .update({content: content, completed: completed});
-  // },
-
-  // delete(username, id) {
-  //   const user_id = knex('plants').select('id').where('username', username);
-  //   // const validateUser = knex('todo').join('plants', 'todo.user_id', '=', 'plants.id').where('todo.user_id', user_id).where('todo.id', id);
-  //   return knex('todo')
-  //     .join('plants', 'todo.user_id', '=', 'plants.id')
-  //     .where('todo.user_id', user_id)
-  //     .where('todo.id', id)
-  //     .del();
-  // },
+  deleteGardenByID(user_id, id){
+    return knex('gardens')
+    .where('id', id)
+    .where('user_id', user_id)
+    .del();
+  },
 };
--- routes/gardens.js
+++ routes/gardens.js
@@ -3,7 +3,7 @@ const Router = require("express").Router();
 const db = require('../controllers/gardens');
 
 
-/* GET All gardens from a user */
+/* GET all gardens from a user */
 Router.get("/", async (req, res) => {
   const { id } = req.body;
 	try {
@@ -14,19 +14,25 @@ Router.get("/", async (req, res) => {
   catch(err) { console.log(err); }
 });
 
+
 /* GET a specific garden (of plants) from a user */
 Router.get("/:id", async (req, res) => {
-  // const { garden_id } = req.body;
+  const { user_id } = req.body;
 
   try {
-    db.getPlantsFromGarden(req.params.id).then(plants => {
-      res.status(200).json(plants);
-    })
+    db.getGardenByID(user_id, req.params.id).then(garden => {
+      if(garden.length > 0){
+        res.status(200).json(garden)
+      } else {
+        res.status(200).send("Invalid garden. User does not own garden.")
+      }
+    });
   }
   catch(err) { console.log(err); }
 });
 
-/* Create new garden */
+
+/* POST new garden */
 Router.post("/", async (req, res) => {
   const { body } = req;
 
@@ -40,13 +46,58 @@ Router.post("/", async (req, res) => {
 
   const { garden_name, user_id } = body;
   
-  console.log("passed checks")
   try {
     db.createGarden(garden_name, user_id).then(
       res.status(200).json({success: true})
     )
 
 	}
+  catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'something weird happened with the API.' });
+  }
+});
+
+
+/* PUT garden */
+Router.put("/:id", async (req, res) => {
+  const { body } = req;
+
+  if (!body.hasOwnProperty('garden_name')) {
+    return res.status(400).json({ error: 'you must supply a field "garden_name" when updating a new garden' });
+  }
+
+  if (!body.hasOwnProperty('user_id')) {
+    return res.status(400).json({ error: 'you must supply a field "user_id" when updating a new garden' });
+  }
+
+  const { garden_name, user_id } = body;
+
+  try {
+    db.updateGardenByID(user_id, req.params.id, garden_name).then(
+      res.status(200).json({success: true})
+    );
+  }
+  catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'something weird happened with the API.' });
+  }
+});
+
+
+/* DEL garden */
+Router.delete("/:id/delete", async(req, res) => {
+  const { body } = req;
+
+  if (!body.hasOwnProperty('user_id')) {
+    return res.status(400).json({ error: 'you must supply a field "user_id" when updating a new garden' });
+  }
+
+  try {
+    db.deleteGardenByID(user_id, req.params.id).then(
+      res.status(200).json({success: true})
+    );
+  }
   catch (e) {
     console.error(e);
     res.status(500).json({ error: 'something weird happened with the API.' });
