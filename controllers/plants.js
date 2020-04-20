242ea5cf0ccb53ff644718d22b32991dff13722e
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-20 14:01:59 -0400

    added plant crud operations

--- controllers/plants.js
+++ controllers/plants.js
@@ -1,36 +1,33 @@
 const knex = require('./knex'); // the connection
 
 module.exports = {
-  getAllPlantsFromUser(user_id){
+  getAllPlantsFromUser(user_id) {
     return knex('plants')
     .where('user_id', user_id)
   },
 
-  addPlant(user_id, garden_id, common_name, scientific_name, trefle_id, duration, outdoor_plant, images, foliage, fruit_or_seed, growth, seed, specifications, family_common_name){
+  addPlant(user_id, garden_id, common_name, scientific_name, trefle_id, duration, outdoor_plant, images, foliage, fruit_or_seed, growth, seed, specifications, family_common_name) {
     return knex('plants')
     .insert({ 
       user_id, garden_id, common_name, scientific_name, trefle_id, duration, outdoor_plant, images, foliage, fruit_or_seed, growth, seed, specifications, family_common_name
     })
   },
 
+  updatePlant(id, garden_id, outdoor_plant, user_id, images) {
+    return knex('plants')
+    .where('id', id)
+    .where('user_id', user_id)
+    .update({ 
+     garden_id,
+     outdoor_plant,
+     images
+    })
+  },  
 
-  // getGardenByID(user_id, id){
-  //   return knex('gardens')
-  //   .where('id', id)
-  //   .where('user_id', user_id)
-  // },
-
-  // updateGardenByID(user_id, id, garden_name){
-  //   return knex('gardens')
-  //   .where('id', id)
-  //   .where('user_id', user_id)
-  //   .update({garden_name: garden_name})
-  // },
-
-  // deleteGardenByID(user_id, id){
-  //   return knex('gardens')
-  //   .where('id', id)
-  //   .where('user_id', user_id)
-  //   .del();
-  // },
+  deletePlant(user_id, id){
+    return knex('plants')
+    .where('id', id)
+    .where('user_id', user_id)
+    .del();
+  },
 };
--- routes/plants.js
+++ routes/plants.js
@@ -41,7 +41,40 @@ Router.post("/", async (req, res) => {
   catch (e) {
     console.error(e);
     res.status(500).json({ error: 'something weird happened with the API.' });
-  }});
+  }
+});
+
+
+/* PUT plant */
+Router.put("/:id", async (req, res) => {
+  const { body } = req;
+
+  if (!body.hasOwnProperty('user_id')) {
+    return res.status(400).json({ error: 'you must supply a field "user_id" when updating a new garden' });
+  }
+
+  if (!body.hasOwnProperty('garden_id')) {
+    return res.status(400).json({ error: 'you must supply a field "garden_id" when updating a new garden' });
+  }
+
+  if (!body.hasOwnProperty('outdoor_plant')) {
+    return res.status(400).json({ error: 'you must supply a field "outdoor_plant" when updating a new garden' });
+  }
+
+  const { garden_id, outdoor_plant, user_id, images } = body;
+
+  try {
+    db.updatePlant(req.params.id, garden_id, outdoor_plant, user_id, images).then(
+      res.status(200).json({success: true})
+    );
+  }
+  catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'something weird happened with the API.' });
+  }
+
+});
+
 
 // /* GET a specific garden (of plants) from a user */
 // Router.get("/:id", async (req, res) => {
@@ -53,32 +86,6 @@ Router.post("/", async (req, res) => {
 //     })
 //   }
 //   catch(err) { console.log(err); }
-// });
-
-// /* Create new garden */
-// Router.post("/", async (req, res) => {
-//   const { body } = req;
-
-//   if (!body.hasOwnProperty('garden_name')) {
-//     return res.status(400).json({ error: 'you must supply a field "garden_name" when creating a new garden' });
-//   }
-
-//   if (!body.hasOwnProperty('user_id')) {
-//     return res.status(400).json({ error: 'you must supply a field "user_id" when creating a new garden' });
-//   }
-
-//   const { garden_name, user_id } = body;
-  
-//   console.log("passed checks")
-//   try {
-//     const newGardenId = await db.createGarden(garden_name, user_id)
-
-   
-// 	}
-//   catch (e) {
-//     console.error(e);
-//     res.status(500).json({ error: 'something weird happened with the API.' });
-//   }
 // });
 
 
