a0c766a826563456e0138b38fa26757a1eeeb7ec
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-04-28 18:47:50 -0400

    Fixed proxying CORS error in header

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const request = require('request');
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, './')));
 
 const usersRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
@@ -19,10 +21,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use("/api/users", usersRoute);
 app.use("/api/gardens", gardensRoute);
 app.use("/api/plants", plantsRoute);
 app.use("/api/auth", authRoute);
+
 
 /* For Deploying */
 // The "catchall" handler: for any request that doesn't
@@ -32,8 +42,9 @@ app.use("/api/auth", authRoute);
 // });
 
 app.set('port', (process.env.PORT || 3001));
+
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`);
-})
+});
 
 module.exports = app;
