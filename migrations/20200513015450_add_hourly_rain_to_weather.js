8bfa44e4aecdf4f93c9f948949b1ef45fec2dd18
Josiah Nosek <Josiah.Nosek69@myhunter.cuny.edu>
2020-05-13 02:09:03 -0400

    fixed promise for table weather alter

--- migrations/20200513015450_add_hourly_rain_to_weather.js
+++ migrations/20200513015450_add_hourly_rain_to_weather.js
@@ -1,15 +1,11 @@
-exports.up = function (knex, Promise) {
-    return Promise.all([
-        knex.schema.table('weather', function (table) {
+exports.up = function (knex) {
+        return knex.schema.table('weather', function (table) {
             table.decimal('hourly_rain', 5, 2);
         })
-    ])
 };
 
-exports.down = function (knex, Promise) {
-    return Promise.all([
-        knex.schema.table('weather', function (table){
+exports.down = function (knex) {
+        return knex.schema.table('weather', function (table){
             table.dropColumn('hourly_rain');
         })
-    ])
 };
